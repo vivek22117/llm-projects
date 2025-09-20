6e9b4d647cf19e2b86df45477c0e552d269a19a8
vivek22117 <vivekmishra22117@gmail.com>
2025-09-20 17:12:47 +0530

    add db connection package

---
+++ llm-rag-app/database/core.py
@@ -0,0 +1,26 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session, declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+"""DATABASE URL from environment variable"""
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+DbSession = Annotated[Session, Depends(get_db)]
