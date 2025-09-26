aed0d14dff99ffffa8828991068639cceae15ce9
vivek22117 <vivekmishra22117@gmail.com>
2025-09-26 22:29:49 +0530

    add model and train

--- titanic-data-process-sklearn/main.py
+++ titanic-data-process-sklearn/main.py
@@ -21,8 +21,8 @@ print(raw_df.isnull().sum())
 def data_preprocessing(df: DataFrame) -> DataFrame:
     df.drop(columns=['PassengerId', 'Name', 'Ticket', 'Cabin'], inplace=True)
 
-    df['Embarked'].fillna('S', inplace=True)
-    df.drop(columns=['Embarked'])
+    df['Embarked'].fillna("S", inplace=True)
+    df.drop(columns=['Embarked'], inplace=True)
 
     fill_missing_ages(df)
 
@@ -45,3 +45,51 @@ def fill_missing_ages(df: DataFrame):
             age_fill_map[pclass] = df[df['Pclass'] == pclass]['Age'].median()
 
     df['Age'] = df.apply(lambda row: age_fill_map[row['Pclass']] if pd.isnull(row['Age']) else row['Age'], axis=1)
+
+
+data = data_preprocessing(raw_df)
+
+# Create features / targets variables (Flash Cards)
+x = data.drop(columns=['Survived'])
+y = data['Survived']
+
+y = y[x.notna().all(axis=1)]
+x.dropna(inplace=True)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
+
+# ML Preprocessing
+scaler = MinMaxScaler()
+x_train_scaled = scaler.fit_transform(x_train)
+x_test_scaled = scaler.transform(x_test)
+
+# HyperParameter Tuning  - KNN Model
+def tune_knn_model(X_train, Y_train) -> KNeighborsClassifier:
+    knn_params = {
+        "n_neighbors": range(1, 21),
+        "metric": ["euclidean", "manhattan", "minkowski"],
+        "weights": ["uniform", "distance"]
+    }
+
+    model = KNeighborsClassifier()
+    grid_search = GridSearchCV(model, param_grid=knn_params, cv=5, n_jobs=-1)
+    grid_search.fit(X_train, Y_train)
+    return grid_search.best_estimator_
+
+best_model = tune_knn_model(x_train_scaled, y_train)
+
+
+# Predictions and Evaluate
+def evaluate_model(model: KNeighborsClassifier, X_test, Y_test):
+    prediction = model.predict(X_test)
+    accuracy = accuracy_score(Y_test, prediction)
+    matrix = confusion_matrix(Y_test, prediction)
+    return accuracy, matrix
+
+accuracy, matrix = evaluate_model(best_model, x_test_scaled, y_test)
+print(f"Accuracy: {accuracy*100:.2f}%")
+print(f"Confusion Matrix:\n{matrix}")
+
+
+
+
